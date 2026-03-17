5bb1554348925f65fb9f2b492dee021c1de2ac7a
Muhammad Nursalim Imron <muhammad.imron@bankina.local>
2026-03-17 18:38:47 +0700

    init dimsum-dotcom

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="bg-primary min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl font-extrabold text-tertiary">Dimsum DotCom</h1>
+        <p className="text-secondary mt-4 font-bold text-xl text-blue-500">Sedang Loading...</p>
+        <div className="mt-8 p-4 bg-white rounded-lg shadow-lg">
+          Varian: Wortel, Jamur, Keju
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
